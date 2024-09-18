4157c3265de3ecc17f65e22a66978e6a20c89c77
josue Avuh <jkomi2003@gmail.com>
2024-09-18 17:34:47 +0100

    sidebar change

--- proteam/src/components/Sidebar/index.tsx
+++ proteam/src/components/Sidebar/index.tsx
@@ -1,11 +1,63 @@
 import React from 'react'
+import {
+  AlertCircle,
+  AlertOctagon,
+  AlertTriangle,
+  Briefcase,
+  ChevronDown,
+  ChevronUp,
+  Home,
+  Layers3,
+  LockIcon,
+  LucideIcon,
+  Search,
+  Settings,
+  ShieldAlert,
+  User,
+  Users,
+  X,
+} from "lucide-react";
+import Image from "next/image";
+import Link from "next/link"
 
 const index = () => {
   return (
-    <div>
-      Sidebar
-    </div>
-  )
-}
+    <div className=""> 
+      <div className="flex h-[100%] w-full flex-col justify-start">
+        {/* TOP LOGO */}
+        <div className="z-50 flex min-h-[56px] w-64 items-center justify-between bg-white px-6 pt-3 dark:bg-black">
+          <div className="text-xl font-bold text-gray-800 dark:text-white">
+            Proteam
+          </div>
+          
+        </div>
 
-export default index
+        {/* TEAM */}
+        <div className="flex items-center gap-5 border-y-[1.5px] border-gray-200 px-8 py-4 dark:border-gray-700">
+          <Image
+            src=""
+            alt="Logo"
+            width={40}
+            height={40}
+          />
+          <div>
+            <h3 className="text-md font-bold tracking-wide dark:text-gray-200">
+              Jojo's Team
+            </h3>
+            <div className="mt-1 flex items-start gap-2">
+              <LockIcon className="mt-[0.1rem] h-3 w-3 text-gray-500 dark:text-gray-400" />
+              <p className="text-xs text-gray-500">Private</p>
+            </div>
+          </div>
+        </div>
+
+        {/* NAVBAR LINKS */}
+        <nav className="z-10 w-full">
+          
+        </nav>
+      </div> 
+    </div> 
+  );
+};
+
+export default index;
