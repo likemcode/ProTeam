2b137a56d935bbc97684fc6e2e29ccbbf527a5e7
josue Avuh <jkomi2003@gmail.com>
2024-09-20 13:44:06 +0100

    navbar and sidebar rework

--- proteam/src/components/Navbar/index.tsx
+++ proteam/src/components/Navbar/index.tsx
@@ -31,10 +31,14 @@ const Navbar = () => {
     <div className="flex items-center justify-between bg-white px-4 py-3 dark:bg-black">
       {/* Search Bar */}
       <div className="flex items-center gap-8">
-      {!isSidebarCollapsed ? null: (
-        <button onClick={() => dispatch(setIsSidebarCollapsed(true))}></button>
-      )}
-          <Menu className="h-8 w-8 cursor-pointer dark:text-white"></Menu>
+        {!isSidebarCollapsed ? null : (
+          <button
+            onClick={() => dispatch(setIsSidebarCollapsed(!isSidebarCollapsed))}
+          >
+            <Menu className="h-8 w-8 dark:text-white" />
+          </button>
+        )}
+          
         <div className="relative flex h-min w-[200px]">
           <Search className="absolute left-[4px] top-1/2 mr-2 h-5 w-5 -translate-y-1/2 transform cursor-pointer dark:text-white" />
           <input
