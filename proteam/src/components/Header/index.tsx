4521c3899c2a9327c0b8e8140a3c89918fcb790b
josue Avuh <jkomi2003@gmail.com>
2024-09-30 16:25:24 +0100

    slight change

--- proteam/src/components/Header/index.tsx
+++ proteam/src/components/Header/index.tsx
@@ -9,6 +9,7 @@ type Props = {
 const Header = ({ name, buttonComponent, isSmallText = false }: Props) => {
   return (
     <div className="mb-5 flex w-full items-center justify-between">
+        
       <h1
         className={`${isSmallText ? "text-lg" : "text-2xl"} font-semibold dark:text-white`}
       >
