d642479ed392cfd3b166b4cb8ee557e8ebaed2cb
josue Avuh <jkomi2003@gmail.com>
2024-10-23 12:27:23 +0100

    added clerk

--- proteam/src/app/dashboardWrapper.tsx
+++ proteam/src/app/dashboardWrapper.tsx
@@ -1,30 +1,32 @@
-"use client"
+"use client";
 
 import React, { useEffect } from "react";
 import Navbar from "@/components/Navbar";
 import Sidebar from "@/components/Sidebar";
 import StoreProvider, { useAppSelector } from "./redux";
+import { SignedIn, SignedOut, RedirectToSignIn } from "@clerk/nextjs";
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   const isSidebarCollapsed = useAppSelector(
-    (state) => state.global.isSidebarCollapsed,
+    (state) => state.global.isSidebarCollapsed
   );
   const isDarkMode = useAppSelector((state) => state.global.isDarkMode);
 
-  useEffect(()=>{
+  useEffect(() => {
     if (isDarkMode) {
       document.documentElement.classList.add("dark");
     } else {
       document.documentElement.classList.remove("dark");
     }
-  })
-
+  }, [isDarkMode]);
 
   return (
     <div className="flex min-h-screen w-full bg-gray-50 text-gray-900">
       <Sidebar />
-      <main className={`flex w-full flex-col bg-gray-50 dark:bg-dark-bg ${isSidebarCollapsed ? 'ml-0' : 'ml-64'}`} 
-       
+      <main
+        className={`flex w-full flex-col bg-gray-50 dark:bg-dark-bg ${
+          isSidebarCollapsed ? "ml-0" : "ml-64"
+        }`}
       >
         <Navbar />
         {children}
@@ -36,8 +38,16 @@ const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
 const DashboardWrapper = ({ children }: { children: React.ReactNode }) => {
   return (
     <StoreProvider>
+      {/* Render DashboardLayout only if the user is signed in */}
+      <SignedIn>
         <DashboardLayout>{children}</DashboardLayout>
-   </StoreProvider>
+      </SignedIn>
+
+      {/* Redirect users to the sign-in page if they are not authenticated */}
+      <SignedOut>
+        <RedirectToSignIn />
+      </SignedOut>
+    </StoreProvider>
   );
 };
 
--- proteam/src/app/layout.tsx
+++ proteam/src/app/layout.tsx
@@ -1,7 +1,9 @@
+// layout.tsx
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import DashboardWrapper from "./dashboardWrapper";
+import { ClerkProvider } from "@clerk/nextjs"; // Import the Next.js version of ClerkProvider
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,10 +18,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        <DashboardWrapper>{children}</DashboardWrapper>
-      </body>
-    </html>
+    <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
+      <html lang="en">
+        <body className={inter.className}>
+          <DashboardWrapper>{children}</DashboardWrapper>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
