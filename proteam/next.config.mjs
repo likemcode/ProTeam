3f5511b23249c63777b1ce288868edc9df03c1de
josue Avuh <jkomi2003@gmail.com>
2024-10-30 12:58:53 +0100

    next file change

--- proteam/next.config.mjs
+++ proteam/next.config.mjs
@@ -1,14 +1,14 @@
-// next.config.mjs
+import path from 'path';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    webpack: (config) => {
-      config.resolve.alias = {
-        ...config.resolve.alias,
-        '@': require('path').resolve(__dirname, 'src'),
-      };
-      return config;
-    },
-  };
-  
-  export default nextConfig;
-  
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(process.cwd(), 'src'), // Corrected for ESM
+    };
+    return config;
+  },
+};
+
+export default nextConfig;
