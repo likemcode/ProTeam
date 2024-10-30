99a218b2eb33d2c1534a812c4a5efb257cfd7e23
josue Avuh <jkomi2003@gmail.com>
2024-10-30 13:39:13 +0100

    good one

--- proteam/next.config.mjs
+++ proteam/next.config.mjs
@@ -1,14 +1,4 @@
-import path from 'path';
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      '@': path.resolve(process.cwd(), 'src'), // Corrected for ESM
-    };
-    return config;
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
