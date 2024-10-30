bdb3b3a334a82badf7e2253b8f66cba35022f1f9
josue Avuh <jkomi2003@gmail.com>
2024-10-30 12:55:50 +0100

    next.conffig.mjs update

--- proteam/next.config.mjs
+++ proteam/next.config.mjs
@@ -1,4 +1,14 @@
+// next.config.mjs
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    webpack: (config) => {
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        '@': require('path').resolve(__dirname, 'src'),
+      };
+      return config;
+    },
+  };
+  
+  export default nextConfig;
+  
